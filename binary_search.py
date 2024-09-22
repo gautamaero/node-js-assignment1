0480e93663e52ebc910d27a82d35d69a4c4c710b
gautam jha <jhagautamkumar362@gmail.com>
2024-09-22 19:55:15 +0530

    binary_search_algorithm

---
+++ binary_search.py
@@ -0,0 +1,17 @@
+def binary_search(arr,target):
+  left=0;
+  right=len(arr)-1;
+  
+  while(left<=right):
+    mid=(left+right)//2
+    
+    if(arr[mid]==target):
+      return mid
+    
+    elif(arr[mid]<target):
+      low=mid+1
+      
+    elif(arr[mid]>target):
+      right=mid-1
+  
+  return -1
